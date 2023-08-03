c9b7c9cce45a3fff37202ec38e557aff1c04edd5
Dwayna Harris <dwaynaharris@gmail.com>
2023-08-03 11:10:46 -0400

    organize js code better

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('form').addEventListener('submit', (event) => {
         event.preventDefault();
-
         let checkBox = document.createElement("INPUT");
         checkBox.setAttribute("type", "checkbox");
         document.querySelector('#listContainer').appendChild(checkBox);
-
         let p = document.createElement('p');
         p.textContent = event.target.tasks.value;
         p.classList.add('taskList');
         document.querySelector('#listContainer').appendChild(p);
     })
+
     document.querySelector('button').addEventListener("click", (event) => {
         event.preventDefault();
         const list = document.getElementById("listContainer");
